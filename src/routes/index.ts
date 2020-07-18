c3a96f8fbc16869d8ad78f44d180a0005c8cc491
Luiz-Fellipe <devluizfellipe@gmail.com>
2020-07-18 12:40:00 -0300

    first commit

---
+++ src/routes/index.ts
@@ -0,0 +1,10 @@
+// src/routes/index.ts
+import { Router } from 'express';
+
+const routes = Router();
+
+routes.get('/', (req, res) => {
+  return res.json({ message: 'Hello Word !' });
+});
+
+export default routes;
