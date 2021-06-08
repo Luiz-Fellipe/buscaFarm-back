aa0729a2e5b3351f92685683f1b34255992f4e92
Luiz Fellipe <devluizfellipe@gmail.com>
2021-06-08 17:28:18 -0300

    fix upload user avatar

--- src/modules/users/infra/http/controllers/AvatarController.ts
+++ src/modules/users/infra/http/controllers/AvatarController.ts
@@ -6,10 +6,10 @@ import UpdateUserAvatarService from '../../../services/UpdateUserAvatarService';
 export default class AvatarController {
   public async create(req: Request, res: Response): Promise<Response> {
     const updateAvatar = container.resolve(UpdateUserAvatarService);
-    const { userId } = req.user as any;
-
+    const { id } = req.user as any;
+    console.log(req.user);
     const user = await updateAvatar.execute({
-      user_id: userId,
+      user_id: id,
       avatarFileName: req.file.filename,
     });
 
